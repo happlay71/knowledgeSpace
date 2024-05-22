1738fc1b0239c2501726a7b25fd22db7a060c076
quyang <1633433173@qq.com>
2024-05-22 19:41:09 +0800

    更新

---
+++ src/main/java/com/happlay/KnowledgeSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.happlay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeSpaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/happlay/KnowledgeSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happlay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
